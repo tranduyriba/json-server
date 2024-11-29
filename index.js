948b4b774b7c2375518cbb64270dbb7f7d6c5835
Tran Ngoc Duy <tranduy.riba@gmail.com>
2024-11-29 17:29:08 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import jsonServer from "json-server";
+import auth from "json-server-auth";
+import cors from "cors";
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+const rules = auth.rewriter({
+	users: 600,
+	messages: 640,
+});
+
+app.db = router.db;
+
+app.use(rules);
+app.use(cors());
+app.use(auth);
+app.use(router);
+app.listen(3000, () => {
+	console.log(`Server is running on 3000 `);
+});
